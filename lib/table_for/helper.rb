5f4c2a6227f47e3f53e6de5cb085e600d6164f1f
Nicklas Ramhöj <ramhoj@gmail.com>
2010-10-05 19:37:25 +0200

    Added some more documentation

--- lib/table_for/helper.rb
+++ lib/table_for/helper.rb
@@ -1,13 +1,38 @@
 module TableFor
   module Helper
-    # Create a table for records, using model class for naming things.
+    # Create a html table for records, using model class for naming things.
     #
-    # Example:
+    # Examples:
     #
-    # table_for User @users do |table|
-    # table.columns :first_name, :last_name
+    # <tt>
+    # table_for Product, @products do |table|
+    #   table.head :name, :size, :description, :price
+    #   table.body do |row|
+    #     row.cell :name
+    #     row.cells :size, :description
+    #     row.cell number_to_currency(row.record.price)
+    #   end
+    #   table.foot do
+    #     link_to "Add product", new_product_path
+    #   end
     # end
+    # </tt>
     #
+    # <tt>
+    # table_for Product, @products do |table|
+    #   table.columns :name, :size, :description, :price
+    #   table.foot do
+    #     link_to "Add product", new_product_path
+    #   end
+    # end
+    # </tt>
+    #
+    # Returns:
+    #
+    # A string containing the html table
+    #
+    # (Call this method from your erb templates by wrapping each line in <%= %> or <% %>)
+
     def table_for(model_class, records, &block)
       Table.new(self, model_class, records, block).render
     end
